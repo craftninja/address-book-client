bd1a64954a21eaaafc4df6aea3586f75619a91d2
Emily Platzer <emilyplatzer@gmail.com>
2018-07-19 20:46:26 -0600

    Removed cruft from search component

--- src/Search.js
+++ src/Search.js
@@ -2,8 +2,6 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Autocomplete } from 'react-md';
 
-const data = [];
-
 export default class Search extends PureComponent {
   static propTypes = {
     onFocus: PropTypes.func,
@@ -12,63 +10,17 @@ export default class Search extends PureComponent {
     value: PropTypes.string,
   };
 
-  state = { filteredData: [] };
-
-  /**
-   * This custom filter will take the current value and return all matches that start
-   * with the value ignoring case and then bold the letters in the search results that
-   * match.
-   */
-  filter = (value) => {
-    const r = new RegExp(`^${value}`, 'i');
-    const l = value.length;
-    const filteredData = data.reduce((matches, state) => {
-      if (r.test(state)) {
-        matches.push({
-          label: [
-            <span key="bold" className="md-font-bold">{state.substring(0, l)}</span>,
-            state.substring(l),
-          ],
-          value: state,
-        });
-      }
-
-      return matches;
-    }, []);
-
-    this.setState({ filteredData });
-  };
-
-  handleChange = (value) => {
-    if (this.props.onChange) {
-      this.props.onChange(value);
-    }
-
-    this.filter(value);
-  };
-
-  handleAutocomplete = (value) => {
-    if (this.props.onAutocomplete) {
-      this.props.onAutocomplete(value);
-    }
-
-    this.filter(value);
-  };
-
   render() {
-    const { filteredData } = this.state;
     return (
       <Autocomplete
         {...this.props}
         id="toolbar-search"
         className="toolbar-search"
         type="search"
-        data={filteredData}
+        data={[]}
         placeholder="For Companies by name"
         customSize="search"
         filter={null}
-        onChange={this.handleChange}
-        onAutocomplete={this.handleAutocomplete}
         block
         dataLabel="label"
         dataValue="value"
