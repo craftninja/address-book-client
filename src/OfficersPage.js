2f62d6563f0647540ff97ea8dcf53e7766816c2c
Emily Platzer <emilyplatzer@gmail.com>
2018-07-20 10:14:21 -0600

    Renamed people to officers

--- src/OfficersPage.js
+++ src/OfficersPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class PeoplePage extends Component {
+export default class OfficerPage extends Component {
   render() {
     return(
       <p>it's peeps time</p>
--- src/constants/pageListItems.js
+++ src/constants/pageListItems.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { FontIcon } from 'react-md';
 import BusinessesPage from '../BusinessesPage';
-import PeoplePage from '../PeoplePage';
+import OfficersPage from '../OfficersPage';
 import SearchPage from '../SearchPage';
 
-export default [{
-  content: <SearchPage/>,
-  key: 'search',
-  primaryText: 'Search',
-  leftIcon: <FontIcon>search</FontIcon>,
-  active: true,
-},
-{ key: 'divider', divider: true },
-{
-  content: <PeoplePage/>,
-  key: 'people',
-  primaryText: 'My Saved People',
-  leftIcon: <FontIcon>people</FontIcon>,
-}, {
-  content: <BusinessesPage/>,
-  key: 'business',
-  primaryText: 'My Saved Businesses',
-  leftIcon: <FontIcon>business</FontIcon>,
-}];
+export default [
+  {
+    content: <SearchPage/>,
+    key: 'search',
+    primaryText: 'Search',
+    leftIcon: <FontIcon>search</FontIcon>,
+    active: true,
+  },
+  { key: 'divider', divider: true },
+  {
+    content: <BusinessesPage/>,
+    key: 'business',
+    primaryText: 'My Saved Businesses',
+    leftIcon: <FontIcon>business</FontIcon>,
+  },
+  {
+    content: <OfficersPage/>,
+    key: 'officers',
+    primaryText: 'My Saved Officers',
+    leftIcon: <FontIcon>people</FontIcon>,
+  },
+];
